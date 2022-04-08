f90c8f2b21a6dc28ed6604346869321670d90af2
OstiZ <93890262+valeron02@users.noreply.github.com>
2022-04-08 22:28:10 +0900

    Flappybird
    Argunov

---
+++ FlappyBird/TheWall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FlappyBird
+{
+    class TheWall
+    {
+        public int x;
+        public int y;
+
+        public int sizeX;
+        public int sizeY;
+
+        public Image wallImg;
+
+        public TheWall(int x, int y,bool isRotatedImage=false)
+        {
+            wallImg = Resource1.tube;
+            this.x = x;
+            this.y = y;
+            sizeX = 50;
+            sizeY = 250;
+            if(isRotatedImage)
+                wallImg.RotateFlip(RotateFlipType.Rotate180FlipX);
+        }
+    }
+}
